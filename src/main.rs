0f42ab634f142a01b33ea4887c291faa0286fd0f
tk1351 <takakikitsui0111@gmail.com>
2024-09-04 18:16:57 +0900

    feat: add first app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new();
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    let addr = listener.local_addr().unwrap();
+
+    println!("listening on {} 🚀", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
