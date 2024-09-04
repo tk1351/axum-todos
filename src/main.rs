25e0bb681e9cc3b364ca359209c245dde683cdb3
tk1351 <takakikitsui0111@gmail.com>
2024-09-04 20:15:26 +0900

    feat: add get and post routes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use axum::Router;
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new();
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().route("/todos", get(todos_index).post(todos_create));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -12,4 +24,36 @@ async fn main() {
     println!("listening on {} 🚀", addr);
 
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn todos_index() -> Json<Vec<Todo>> {
+    let todos = vec![Todo {
+        id: Uuid::new_v4(),
+        text: String::from("Sample Todo"),
+        completed: false,
+    }];
+
+    Json(todos)
+}
+
+#[derive(Debug, Deserialize)]
+struct CreateTodo {
+    text: String,
+}
+
+async fn todos_create(Json(input): Json<CreateTodo>) -> impl IntoResponse {
+    let todo = Todo {
+        id: Uuid::new_v4(),
+        text: input.text,
+        completed: false,
+    };
+
+    (StatusCode::CREATED, Json(todo))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Todo {
+    id: Uuid,
+    text: String,
+    completed: bool,
 }
